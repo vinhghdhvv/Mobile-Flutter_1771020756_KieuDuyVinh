daae21bcc7d6ea381120288ebbcc9272af099431
vinhghdhvv <vinhquang123423@gmail.com>
2026-02-02 00:08:39 +0700

    Delete pcm_mobile directory

--- pcm_mobile/android/app/src/main/kotlin/com/example/pcm_mobile/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.pcm_mobile
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- pcm_mobile/lib/core/api.dart
+++
@@ -1,3 +0,0 @@
-class Api {
-  static const String baseUrl = "http://10.0.2.2:5081/api";
-}
