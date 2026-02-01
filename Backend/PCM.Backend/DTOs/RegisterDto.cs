7db45df48fb0c3191e76255b8b14c6cfec51f751
vinhghdhvv <vinhquang123423@gmail.com>
2026-02-02 00:08:20 +0700

    Delete Backend/PCM.Backend directory

--- Backend/PCM.Backend/DTOs/RegisterDto.cs
+++
@@ -1,8 +0,0 @@
-namespace PCM.Backend.Dtos
-{
-    public class RegisterDto
-    {
-        public string Email { get; set; } = string.Empty;
-        public string Password { get; set; } = string.Empty;
-    }
-}
--- Backend/PCM.Backend/Models/ApplicationUser.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Identity;
-
-namespace PCM.Backend.Models
-{
-    public class ApplicationUser : IdentityUser
-    {
-        // Sau này có thể mở rộng
-    }
-}
